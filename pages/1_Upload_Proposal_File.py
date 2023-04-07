979f419fa1344c8ea9e10e147de6da91d52de8fd
Meet Doshi <doshi.mee@northeastern.edu>
2023-04-07 10:32:29 -0400

    updated dashboard page

--- pages/1_Upload_Proposal_File.py
+++ pages/1_Upload_Proposal_File.py
@@ -10,23 +10,51 @@ st.write('')
 st.subheader('Upload your investment proposal and get an evaluation of its viability.')
 st.write('')
 
-# Setting up the client
-client, processor_name, credentials = setup_client()
+if 'uploaded_file' not in st.session_state:
+    st.session_state.uploaded_file = ''
+    st.session_state.client = ''
+    st.session_state.processor_name = ''
+    st.session_state.credentials = ''
+    st.session_state.information = ''
+    st.session_state.processed_text = ''
 
 # Creating a file uploader widget that allows users to upload only certain file types
 uploaded_file = st.file_uploader("Choose a file", type=["pdf", "tif", "tiff", "png", "jpg", "jpeg"])
+st.session_state.uploaded_file = uploaded_file
 
-# When the "Evaluate" button is clicked, process the file and display the text
-if st.button("Evaluate"):
-    if uploaded_file is not None:
-        # Determine the mime type of the uploaded file
-        mime_type = mimetypes.guess_type(uploaded_file.name)[0]
-        # Check if the mime type is supported
-        if mime_type not in ['application/pdf', 'image/tiff', 'image/png', 'image/jpeg']:
-            st.warning("Please upload a supported file type (PDF, TIFF, PNG, JPG).")
+if st.session_state.uploaded_file is not None:
+    
+    # When the "Evaluate" button is clicked, process the file and display the text
+    if st.button("Evaluate"):
+        
+        # Setting up the client
+        client, processor_name, credentials = setup_client()
+        st.session_state.client = client
+        st.session_state.processor_name = processor_name
+        st.session_state.credentials = credentials
+
+        if uploaded_file is not None:
+            
+            # Determine the mime type of the uploaded file
+            mime_type = mimetypes.guess_type(uploaded_file.name)[0]
+            
+            # Check if the mime type is supported
+            if mime_type not in ['application/pdf', 'image/tiff', 'image/png', 'image/jpeg']:
+                st.warning("Please upload a supported file type (PDF, TIFF, PNG, JPG).")
+            
+            else:
+                # If the file type is supported, process the file
+                processed_text = process_file(mime_type, client, processor_name, uploaded_file, credentials)
+                st.session_state.processed_text = processed_text
+
+                information = extract_info(processed_text)
+                st.session_state.information = information
+                st.success("Your proposal has been evaluated.")
         else:
-            # If the file type is supported, process the file
-            process_file(mime_type, client, processor_name, uploaded_file, credentials)
-    else:
-        # If no file is uploaded, display an error message
-        st.error("No file uploaded.")
+            # If no file is uploaded, display an error message
+            st.error("No file uploaded.")
+            st.experimental_rerun()
+
+if st.session_state.uploaded_file is None:
+    st.warning('Please Upload the file to proceed !!!')
+    st.experimental_rerun()
--- pages/2_Proposal_Dashboard.py
+++ pages/2_Proposal_Dashboard.py
@@ -1,7 +1,26 @@
 # Importing necessary modules
 import streamlit as st
+from main_functions import create_graph, create_donut_chart, evaluate_investability
 
 # Setting the title and header of the application
 st.title('StealTheDeal')
 st.header('# Seal the Deal on Your Next Investment')
 st.write('')
+
+if st.session_state.information is not None:
+    # If the user has already entered information, we will use it
+    information = st.session_state.information
+    processed_text = st.session_state.processed_text
+    
+    fig = create_graph(information)
+    st.plotly_chart(fig)
+    
+    investability = evaluate_investability(processed_text)
+    st.write('Investability Score:',investability)
+    st.write('')
+    
+    donut_chart = create_donut_chart(investability)
+    st.plotly_chart(donut_chart)
+
+if st.session_state.information is None:
+    st.warning('Please see if the file is evaluated successfully !!!')
